f8dfebada10557dd8d7a65c191eef0ad38122ca1
kiri <loga0060@gmail.com>
2022-07-06 18:52:22 -0500

    Create test_kiri_adf_dag.py

---
+++ tests/dags/test_kiri_adf_dag.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+
+from airflow.models import DAG, BaseOperator
+
+try:
+    from airflow.operators.empty import EmptyOperator
+except ModuleNotFoundError:
+    from airflow.operators.dummy import DummyOperator as EmptyOperator
+from airflow.providers.microsoft.azure.operators.data_factory import AzureDataFactoryRunPipelineOperator
+from airflow.providers.microsoft.azure.sensors.data_factory import AzureDataFactoryPipelineRunStatusSensor
+from airflow.utils.edgemodifier import Label
+
+with DAG(
+    dag_id="example_adf_run_pipeline",
+    start_date=datetime(2021, 8, 13),
+    schedule_interval="@daily",
+    catchup=False,
+    default_args={
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3),
+        "azure_data_factory_conn_id": "azure_data_factory",
+        "factory_name": "my-data-factory",  # This can also be specified in the ADF connection.
+        "resource_group_name": "my-resource-group",  # This can also be specified in the ADF connection.
+    },
+    default_view="graph",
+) as dag:
+    begin = EmptyOperator(task_id="begin")
+    end = EmptyOperator(task_id="end")
+
+    # [START howto_operator_adf_run_pipeline]
+    run_pipeline1: BaseOperator = AzureDataFactoryRunPipelineOperator(
+        task_id="run_pipeline1",
+        pipeline_name="pipeline1",
+        parameters={"myParam": "value"},
+    )
+    # [END howto_operator_adf_run_pipeline]
+
+    # [START howto_operator_adf_run_pipeline_async]
+    run_pipeline2: BaseOperator = AzureDataFactoryRunPipelineOperator(
+        task_id="run_pipeline2",
+        pipeline_name="pipeline2",
+        wait_for_termination=False,
+    )
+
+    pipeline_run_sensor: BaseOperator = AzureDataFactoryPipelineRunStatusSensor(
+        task_id="pipeline_run_sensor",
+        run_id=run_pipeline2.output["run_id"],
+    )
+    # [END howto_operator_adf_run_pipeline_async]
+
+    begin >> Label("No async wait") >> run_pipeline1
+    begin >> Label("Do async wait with sensor") >> run_pipeline2
+    [run_pipeline1, pipeline_run_sensor] >> end
